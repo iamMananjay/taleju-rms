64c6f88625c3e316d3af2ffe7d7c6a267c2dc372
AmritGhartiMagar <mananjaysth111@gmail.com>
2025-10-10 12:25:12 +0100

    Initial commit: dev setup

---
+++ taleju-rms/src/main/java/com/taleju/rms/TalejuRmsApplication.java
@@ -0,0 +1,13 @@
+package com.taleju.rms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalejuRmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalejuRmsApplication.class, args);
+	}
+
+}
---
+++ taleju-rms/src/test/java/com/taleju/rms/TalejuRmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taleju.rms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalejuRmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
